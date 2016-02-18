a8fb7cbfeb08b5aa197c28e4e88c045e9853300b
Primož Gabrijelčič <primoz@gabrijelcic.org>
2016-02-18 16:37:06 +0100

    fixed typo in ifdef

--- FastMM4.pas
+++ FastMM4.pas
@@ -9692,7 +9692,7 @@ begin
     while LockCmpxchg(0, 1, @ExpectedMemoryLeaksListLocked) <> 0 do
     begin
 {$ifdef NeverSleepOnThreadContention}
-  {$ifdef UseSwitchToThread9}
+  {$ifdef UseSwitchToThread}
       SwitchToThread;
   {$endif}
 {$else}
