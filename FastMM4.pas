b09f74b32f6a5c6668a7baf3fcf67097f8007f84
Pierre le Riche <pierre@cimso.com>
2026-08-18 16:32:22 +0200

    #104 Add guards in the debug mode GetMem and ReallocMem handlers to catch negative block size requests. This is particularly necessary because the X86 assembly language System.AllocMem and System._ReallocMem do not block negative values from reaching the underlying memory manager like the PurePascal (and thus X64) code path does. (Thanks to Maxim Masiutin.)

--- FastMM4.pas
+++ FastMM4.pas
@@ -9653,6 +9653,15 @@ begin
   {Scan the entire memory pool first?}
   if FullDebugModeScanMemoryPoolBeforeEveryOperation then
     ScanMemoryPoolForCorruptions;
+
+  {The 32-bit assembly language System.AllocMem lets negative size requests through.  Additionally, both the 32-bit and
+  64-bit paths are vulnerable to bad size requests when called from System.GetMemory.}
+  if ASize < 0 then
+  begin
+    Result := nil;
+    Exit;
+  end;
+
   {Enter the memory manager: block scans may not be performed now}
   StartChangingFullDebugModeBlock;
   try
@@ -9879,6 +9888,14 @@ begin
   {Is the debug info surrounding the block valid?}
   if CheckBlockBeforeFreeOrRealloc(LActualBlock, boReallocMem) then
   begin
+    {The 32-bit assembly language System.AllocMem and System._ReallocMem let through negative size requests.
+    Additionally, both the 32-bit and 64-bit paths are vulnerable to bad size requests when called from
+    System.ReallocMemory.}
+    if ANewSize < 0 then
+    begin
+      Result := nil;
+      Exit;
+    end;
     {Get the current block size}
     LBlockSpace := GetAvailableSpaceInBlock(LActualBlock);
     {Can the block fit? We need space for the debug overhead and the block header
